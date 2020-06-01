790e66d7c1a26ab196f3f06bd8ea2d56e7786af8
Vacumsio <vacumsio@gmail.com>
2020-06-01 15:00:50 +0300

    Переъожу к созданию представления

--- WebStoreApp/Controllers/EmployeesController.cs
+++ WebStoreApp/Controllers/EmployeesController.cs
@@ -1,20 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using WebStoreApp.Infrastructure.Interfaces;
+using WebStoreApp.Models;
 
 namespace WebStoreApp.Controllers
 {
     public class EmployeesController : Controller
     {
-        private readonly IEmployeeData _EmployeeData;
+        private readonly IEmployeeData _EmployeesData;
         public EmployeesController(IEmployeeData EmployeeData)
         {
-            _EmployeeData = EmployeeData;
+            _EmployeesData = EmployeeData;
         }
-        public IActionResult Index() => View(_EmployeeData);
+        public IActionResult Index() => View(_EmployeesData);
 
         public IActionResult EmployeeDetails(int id)
         {
-            var employee = _EmployeeData.GetById(id);
+            var employee = _EmployeesData.GetById(id);
             if (employee is null)
             {
                 return NotFound();
@@ -22,5 +24,59 @@ namespace WebStoreApp.Controllers
             return View(employee);
         }
         
+        public IActionResult Edit(int? Id)
+        {
+            if (Id is null) 
+            { 
+                return View(new EmployeeViewModel()); 
+            }
+            if (Id<0)
+            {
+                return BadRequest();
+            }
+
+            var employee = _EmployeesData.GetById((int)Id);
+            if (employee is null)
+            {
+                return NotFound();
+            }
+
+            return View(new EmployeeViewModel
+            {
+                Id = employee.Id,
+                Firstname = employee.Firstname,
+                Surname = employee.Surname,
+                Patronymic = employee.Patronymic,
+                Age = employee.Age
+            }); 
+        }
+
+        public IActionResult Edit(EmployeeViewModel Model)
+        {
+            if (Model is null)
+            {
+                throw new ArgumentNullException(nameof(Model));
+            }
+
+            var employee = new Employee
+            {
+                Id = Model.Id,
+                Firstname = Model.Firstname,
+                Surname = Model.Surname,
+                Patronymic = Model.Patronymic,
+                Age = Model.Age
+            };
+
+            if (Model.Id==0)
+            {
+                _EmployeesData.Add(employee);
+            }
+            else
+            {
+                _EmployeesData.Edit(employee);
+            }
+            _EmployeesData.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
