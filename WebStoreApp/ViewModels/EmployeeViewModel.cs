be6b3dd4f31f4981d1b4dc2c6077d661926bf03d
Vacumsio <vacumsio@gmail.com>
2020-06-04 13:52:45 +0300

    настройка валидации и пул реквест

--- WebStoreApp/ViewModels/EmployeeViewModel.cs
+++ WebStoreApp/ViewModels/EmployeeViewModel.cs
@@ -2,12 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WebStoreApp.Models
 {
     public class EmployeeViewModel
     {
+        [HiddenInput(DisplayValue = false)]
         public int Id { get; set; }
+        [Display(Name = "Имя")]
+        [Required(ErrorMessage = "Имя обязательно")]
+        [StringLength(30, MinimumLength = 2, ErrorMessage = "Длина имени должна быть от 2 до 30 символов")]
         public string Firstname { get; set; }
         public string Surname { get; set; }
         public string Patronymic { get; set; }
