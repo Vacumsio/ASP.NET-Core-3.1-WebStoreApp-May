f91ba74d45faba9ac19ca1a41a6887b84d100066
Vacumsio <vacumsio@gmail.com>
2020-06-04 18:32:55 +0300

    исправление ошибок "нулабл" типов, мелкие изменения, реализация контроллера Шоп /*проблема раздвижки каталога товаров*/ ищу решение

--- WebStoreApp.Domain/Entities/ProuctFilter.cs
+++ WebStoreApp.Domain/Entities/ProuctFilter.cs
@@ -6,8 +6,8 @@ namespace WebStoreApp.Domain.Entities
 {
     public class ProductFilter
     {
-        public int SectionId { get; set; }
+        public int? SectionId { get; set; }
 
-        public int BrandId { get; set; }
+        public int? BrandId { get; set; }
     }
 }
--- WebStoreApp/Controllers/ShopController.cs
+++ WebStoreApp/Controllers/ShopController.cs
@@ -3,11 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebStoreApp.Domain.Entities;
+using WebStoreApp.Infrastructure.Interfaces;
+using WebStoreApp.ViewModels;
 
 namespace WebStoreApp.Controllers
 {
     public class ShopController : Controller
     {
-        public IActionResult Index() => View();
+        private readonly IProductData _ProductData;
+
+        public ShopController(IProductData ProductData) => _ProductData = ProductData;
+
+        public IActionResult Index(int? SectionId, int? BrandId)
+        {
+            var filter = new ProductFilter
+            {
+                SectionId = SectionId,
+                BrandId = BrandId
+            };
+
+            var products = _ProductData.GetProducts(filter);
+
+            return View(new CatalogViewModel
+            {
+                SectionId = SectionId,
+                BrandId = BrandId,
+                Products = products.Select(p => new ProductViewModel
+                {
+                    Id = p.Id,
+                    Name = p.Name,
+                    Order = p.Order,
+                    Price = p.Price,
+                    ImageUrl = p.ImageUrl
+                }).OrderBy(p => p.Order)
+            });
+        }
     }
 }
