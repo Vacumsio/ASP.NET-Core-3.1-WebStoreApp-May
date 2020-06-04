0f82d0d3ae6d28da4e27b1e9936e6faedbed0c08
Vacumsio <vacumsio@gmail.com>
2020-06-04 15:54:37 +0300

    Добавлен интерфейс на продукт, реализация интерфейса с запросом на данные тестдата, и обновлен стартап(добавлена регастрация этого сервиса)

---
+++ WebStoreApp/Infrastructure/Interfaces/IProductData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStoreApp.Domain.Entities;
+
+namespace WebStoreApp.Infrastructure.Interfaces
+{
+    public interface IProductData
+    {
+        IEnumerable<Section> GetSections();
+        IEnumerable<Brand> GetBrands();
+    }
+}
---
+++ WebStoreApp/Infrastructure/Services/InMemoryProductData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStoreApp.Data;
+using WebStoreApp.Domain.Entities;
+using WebStoreApp.Infrastructure.Interfaces;
+
+namespace WebStoreApp.Infrastructure.Services
+{
+    public class InMemoryProductData : IProductData
+    {
+        public IEnumerable<Brand> GetBrands() => TestData.Brands;
+
+        public IEnumerable<Section> GetSections() => TestData.Sections;
+    }
+}
