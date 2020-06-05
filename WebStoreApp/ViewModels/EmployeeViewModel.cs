39be851ddac0ac29d3ac9911bae0267a85277354
Vacumsio <vacumsio@gmail.com>
2020-06-05 11:15:00 +0300

    Исправлена ошибка развертки

--- WebStoreApp/ViewModels/EmployeeViewModel.cs
+++ WebStoreApp/ViewModels/EmployeeViewModel.cs
@@ -11,19 +11,23 @@ namespace WebStoreApp.Models
     {
         [HiddenInput(DisplayValue = false)]
         public int Id { get; set; }
+
         [Display(Name = "Имя")]
         [Required(ErrorMessage = "Имя обязательно")]
         [StringLength(30, MinimumLength = 2, ErrorMessage = "Длина имени должна быть от 2 до 30 символов")]
         [RegularExpression(@"([А-ЯЁ][а-яё]+)|([A-Z][a-z]+)", ErrorMessage = "Ошибка формата имени")]
         public string Firstname { get; set; }
+
         [Display(Name = "Фамилия")]
         [Required(ErrorMessage = "Фамилия обязательна")]
         [StringLength(60, MinimumLength = 3, ErrorMessage = "Длина фамилии должна быть от 3 до 60 символов")]
         [RegularExpression(@"([А-ЯЁ][а-яё]+)|([A-Z][a-z]+)", ErrorMessage = "Ошибка формата Фамилии")]
         public string Surname { get; set; }
+
         [Display(Name = "Отчество")]
         [RegularExpression(@"([А-ЯЁ][а-яё]+)|([A-Z][a-z]+)", ErrorMessage = "Ошибка формата Отчества")]
         public string Patronymic { get; set; }
+
         [Display(Name = "Возраст")]
         [Required]
         [Range(18,100, ErrorMessage ="Возраст должен быть неменьше 18 и не больше 100")]
