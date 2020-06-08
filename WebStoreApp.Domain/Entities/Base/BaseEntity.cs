a336b8559f90013fa6917ae81c94954d9e66cd8e
Vacumsio <vacumsio@gmail.com>
2020-06-08 13:10:26 +0300

    Проблема миграций и праймари ключа

--- WebStoreApp.Domain/Entities/Base/BaseEntity.cs
+++ WebStoreApp.Domain/Entities/Base/BaseEntity.cs
@@ -1,9 +1,12 @@
-﻿using WebStoreApp.Domain.Entities.Base.Interfaces;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using WebStoreApp.Domain.Entities.Base.Interfaces;
 
 namespace WebStoreApp.Domain.Entities.Base
 {
     public abstract class BaseEntity : IBaseEntity
     {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
     }
 }
