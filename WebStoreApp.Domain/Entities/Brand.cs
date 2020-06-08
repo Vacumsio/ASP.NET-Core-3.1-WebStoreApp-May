5a47af6e0ff14a3a8cde879f1ab532d48a3ca76f
Vacumsio <vacumsio@gmail.com>
2020-06-08 11:11:45 +0300

    Исправлены ошибки запущена первая миграция

--- WebStoreApp.Domain/Entities/Brand.cs
+++ WebStoreApp.Domain/Entities/Brand.cs
@@ -1,8 +1,10 @@
-﻿using WebStoreApp.Domain.Entities.Base;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using WebStoreApp.Domain.Entities.Base;
 using WebStoreApp.Domain.Entities.Base.Interfaces;
 
 namespace WebStoreApp.Domain.Entities
 {
+    [Table("ProductBrand")]
     public class Brand: NamedEntity, IOrderEntity
     {
         public int Order { get; set; }
--- WebStoreApp.Domain/Entities/Product.cs
+++ WebStoreApp.Domain/Entities/Product.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Text;
 using WebStoreApp.Domain.Entities.Base;
 using WebStoreApp.Domain.Entities.Base.Interfaces;
 
@@ -10,16 +7,12 @@ namespace WebStoreApp.Domain.Entities
 {
     public class Product : NamedEntity, IOrderEntity
     {
-
-
-
         public int Order { get; set; }
 
         public int SectionId { get; set; }
 
         [ForeignKey(nameof(SectionId))]
         public virtual Section Section { get; set; }
-
         public int? BrandId { get; set; }
 
         [ForeignKey(nameof(BrandId))]
@@ -27,7 +20,7 @@ namespace WebStoreApp.Domain.Entities
 
         [Required]
         public string ImageUrl { get; set; }
-        [Column(TypeName ="decimal(18,2)")]
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
     }
 }
