aced3c9f158a61e0f2ce61299ee41441e18480d8
Vacumsio <vacumsio@gmail.com>
2020-06-11 17:02:50 +0300

    логин готов

--- WebStoreApp/Controllers/AccountController.cs
+++ WebStoreApp/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Identity;
 using WebStoreApp.Domain.Entities.Identity;
 using WebStoreApp.ViewModels.Identity;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace WebStoreApp.Controllers
 {
@@ -47,7 +48,35 @@ namespace WebStoreApp.Controllers
             return View(Model);
         }
 
-        public IActionResult Login() => View();
+        public IActionResult Login(string ReturnUrl) => View(new LoginViewModel{ReturnUrl = ReturnUrl });
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel Model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(Model);
+            }
+
+            var login_result = await _SignInManager.PasswordSignInAsync(
+                Model.UserName,
+                Model.Password,
+                Model.RememberMe,
+                true);
+            if (login_result.Succeeded)
+            {
+                if (Url.IsLocalUrl(Model.ReturnUrl))
+                {
+                    return Redirect(Model.ReturnUrl);
+                }
+
+                return RedirectToAction("Index", "Home");
+            }
+            ModelState.AddModelError(string.Empty, "Неверное имя пользователя или пароль");
+
+            return View(Model);
+        }
+
         public IActionResult Logout() => View();
         public IActionResult AccessDenied() => View();
     }
--- WebStoreApp/ViewModels/Identity/LoginViewModel.cs
+++ WebStoreApp/ViewModels/Identity/LoginViewModel.cs
@@ -1,11 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebStoreApp.ViewModels.Identity
 {
     public class LoginViewModel
     {
+        [Required]
+        [MaxLength(256)]
+        [Display(Name ="Имя")]
+        public string UserName { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+        [Display(Name = "Запомнить меня")]
+        public bool RememberMe { get; set; }
+        [HiddenInput(DisplayValue = false)]
+        public string ReturnUrl { get; set; }
     }
 }
