37963cc3dec9de5f31592fbee77ad0300a6e7165
Vacumsio <vacumsio@gmail.com>
2020-06-11 13:00:17 +0300

    добавил сервис для работы с Работниками

---
+++ WebStoreApp/Infrastructure/Services/InSQL/SqlEmployeeData.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStoreApp.DAL.Context;
+using WebStoreApp.Infrastructure.Interfaces;
+using WebStoreApp.Models;
+
+namespace WebStoreApp.Infrastructure.Services.InSQL
+{
+    public class SqlEmployeeData : IEmployeesData
+    {
+        public readonly WebStoreDB _db;
+
+        public SqlEmployeeData(WebStoreDB db) => _db = db;
+        public int Add(Employee Employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(Employee Employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Employee> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
