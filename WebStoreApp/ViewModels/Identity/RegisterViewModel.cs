c99ae93aece1a039d4eb79dfb3845cf6b63859e4
Vacumsio <vacumsio@gmail.com>
2020-06-11 16:17:59 +0300

    форма регистрации

--- WebStoreApp/ViewModels/Identity/RegisterViewModel.cs
+++ WebStoreApp/ViewModels/Identity/RegisterViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,17 @@ namespace WebStoreApp.ViewModels.Identity
 {
     public class RegisterViewModel
     {
+        [Required]
+        [MaxLength(256)]
+        [Display(Name = "Введите Имя")]
+        public string UserName { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name="Введите пароль")]
+        public string Password { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Подтвердите пароль")]
+        public string ConfirmPassword { get; set; }
     }
 }
