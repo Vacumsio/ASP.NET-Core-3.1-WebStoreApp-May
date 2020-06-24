4d64f0a9e0c80268215422ecd396be0f2f5d6a12
Vacumsio <vacumsio@gmail.com>
2020-06-24 16:20:03 +0300

    Добавлена возможность оформления заказа. Только для ЗАРЕГИСТРИРОВАННОГО пользователя

--- WebStoreApp/Infrastructure/Interfaces/IOrderService.cs
+++ WebStoreApp/Infrastructure/Interfaces/IOrderService.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebStoreApp.Domain.Entities.Orders;
+using WebStoreApp.ViewModels;
 
 namespace WebStoreApp.Infrastructure.Interfaces
 {
-    interface IOrderService
+    public interface IOrderService
     {
+        Task<Order> CreateOrder(string UserName,CartViewModel Cart, OrderViewModel Order);
+        Task<IEnumerable<Order>> GetUserOrders(string UserName);
+
+        Task<Order> GetOrderById(int id);
     }
 }
--- WebStoreApp/Infrastructure/Services/InSQL/SqlOrderService.cs
+++ WebStoreApp/Infrastructure/Services/InSQL/SqlOrderService.cs
@@ -1,12 +1,78 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebStoreApp.DAL.Context;
+using WebStoreApp.Domain.Entities.Identity;
+using WebStoreApp.Domain.Entities.Orders;
 using WebStoreApp.Infrastructure.Interfaces;
+using WebStoreApp.ViewModels;
 
 namespace WebStoreApp.Infrastructure.Services.InSQL
 {
-    public class SqlOrderService: IOrderService
+    public class SqlOrderService : IOrderService
     {
+        private readonly WebStoreDB _db;
+        private readonly UserManager<User> _UserManager;
+
+        public SqlOrderService(WebStoreDB db, UserManager<User> UserManager)
+        {
+            _db = db;
+            _UserManager = UserManager;
+        }
+        public async Task<Order> CreateOrder(string UserName, CartViewModel Cart, OrderViewModel OrderModel)
+        {
+            var user = await _UserManager.FindByNameAsync(UserName);
+            if (user is null)
+            {
+                throw new InvalidOperationException($"льзователь {UserName} не найден");
+            }
+
+            await using var transaction = await _db.Database.BeginTransactionAsync();
+
+            var order = new Order
+            {
+                Name = OrderModel.Name,
+                Phone = OrderModel.Phone,
+                Address = OrderModel.Address,
+                User = user,
+                Date = DateTime.Now,
+                Items = new List<OrderItem>()
+            };
+
+            foreach (var (product_model,quantity) in Cart.Items)
+            {
+                var product = await _db.Products.FindAsync(product_model.Id);
+                if (product is null)
+                {
+                    throw new InvalidOperationException($"Товар {product_model.Id} не найден");
+                }
+                var order_item = new OrderItem
+                {
+                    Order = order,
+                    Price = product.Price,
+                    Quantity = quantity,
+                    Product = product
+                };
+            }
+
+            await _db.Orders.AddAsync(order);
+            await _db.SaveChangesAsync();
+            await transaction.CommitAsync();
+
+            return order;
+        }
+
+        public Task<Order> GetOrderById(int id) => _db.Orders
+            .Include(order => order.Items)
+            .FirstOrDefaultAsync(order => order.Id == id);
+
+        public async Task<IEnumerable<Order>> GetUserOrders(string UserName) => await _db.Orders
+            .Include(order => order.User)
+            .Include(order => order.Items)
+            .Where(order => order.User.UserName == UserName)
+            .ToArrayAsync();
     }
 }
