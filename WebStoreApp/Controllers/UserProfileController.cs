a9b138e15a42976d5c687853cec2aa19a85a8eb9
Vacumsio <vacumsio@gmail.com>
2020-06-24 17:26:00 +0300

    Добавлен профиль пользователя, проблема с Подсчетом Итоговой суммы

---
+++ WebStoreApp/Controllers/UserProfileController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebStoreApp.Infrastructure.Interfaces;
+using WebStoreApp.ViewModels;
+
+namespace WebStoreApp.Controllers
+{
+    public class UserProfileController : Controller
+    {
+        public IActionResult Index() => View();
+
+        public async Task<IActionResult> Orders([FromServices] IOrderService OrderService)
+        {
+            var orders = await OrderService.GetUserOrders(User.Identity.Name);
+
+            return View(orders.Select(order => new UserOrderViewModel
+            {
+               Id =order.Id,
+               Address = order.Address,
+               Name = order.Name,
+               Phone = order.Phone,
+               TotalSum = order.Items.Sum(i => i.Price * i.Quantity)
+            }));
+        }
+    }
+}
