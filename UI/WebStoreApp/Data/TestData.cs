5aa99ef83d9a3eea75b0b60ecf4c7fddd0807f1e
Vacumsio <vacumsio@gmail.com>
2020-06-25 18:23:37 +0300

    пространства имен для ВьюМоделей обновлены

--- UI/WebStoreApp/Data/TestData.cs
+++ UI/WebStoreApp/Data/TestData.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using WebStoreApp.Domain.Entities;
-using WebStoreApp.ViewModels;
 using WebStoreApp.Domain.Entities.Employees;
 
 namespace WebStoreApp.Data
