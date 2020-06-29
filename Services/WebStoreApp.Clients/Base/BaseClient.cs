8c116a0d864928657566dcaf74eeca4b618a8cfb
Vacumsio <vacumsio@gmail.com>
2020-06-29 17:16:29 +0300

    Обновлен Базовый клиент(асинхронный КРУД)

--- Services/WebStoreApp.Clients/Base/BaseClient.cs
+++ Services/WebStoreApp.Clients/Base/BaseClient.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace WebStoreApp.Clients.Base
 {
@@ -25,5 +27,29 @@ namespace WebStoreApp.Clients.Base
                 }
             };
         }
+
+        public T Get<T>(string url) => GetAsync<T>(url).Result;
+        public async Task<T> GetAsync<T>(string url)
+        {
+            var response = await _Client.GetAsync(url);
+            return await response.EnsureSuccessStatusCode().Content.ReadAsAsync<T>();
+        }
+
+        public HttpResponseMessage Post<T>(string url, T item) => PostAsync(url, item).Result;
+        public async Task<HttpResponseMessage> PostAsync<T>(string url, T item)
+        {
+            var response = await _Client.PostAsJsonAsync(url, item);
+            return response.EnsureSuccessStatusCode();
+        }
+
+        public HttpResponseMessage Put<T>(string url, T item) => PutAsync(url, item).Result;
+        public async Task<HttpResponseMessage> PutAsync<T>(string url, T item)
+        {
+            var response = await _Client.PutAsJsonAsync(url, item);
+            return response.EnsureSuccessStatusCode();
+        }
+
+        public HttpResponseMessage Delete<T>(string url) => DeleteAsync(url).Result;
+        public async Task<HttpResponseMessage> DeleteAsync(string url) => await _Client.DeleteAsync(url);
     }
 }
---
+++ Services/WebStoreApp.Clients/Employees/EmployeesClient.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using WebStoreApp.Clients.Base;
+using WebStoreApp.Domain;
+using WebStoreApp.Domain.Entities.Employees;
+using WebStoreApp.Interfaces.Services;
+
+namespace WebStoreApp.Clients.Employees
+{
+    public class EmployeesClient : BaseClient, IEmployeesData
+    {
+        public EmployeesClient(IConfiguration Configuration) : base(Configuration, WebApi.Employees) { }
+
+        public int Add(Employee Employee) => throw new NotImplementedException();
+
+        public bool Delete(int id) => throw new NotImplementedException();
+
+        public void Edit(Employee Employee) => throw new NotImplementedException();
+
+        public IEnumerable<Employee> Get() => throw new NotImplementedException();
+
+        public Employee GetById(int id) => throw new NotImplementedException();
+
+        public void SaveChanges() => throw new NotImplementedException();
+    }
+}
