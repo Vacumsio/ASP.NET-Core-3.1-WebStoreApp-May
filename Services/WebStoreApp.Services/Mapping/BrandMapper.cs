a01c2cccf2d4be6195a3a139096f70e6b52c3073
Vacumsio <vacumsio@gmail.com>
2020-06-30 16:36:47 +0300

    Настроил мапперы для бренда, заказа, товара, секции

---
+++ Services/WebStoreApp.Services/Mapping/BrandMapper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebStoreApp.Domain.DTO;
+using WebStoreApp.Domain.Entities;
+
+namespace WebStoreApp.Services.Mapping
+{
+    public static class BrandMapper
+    {
+        public static BrandDTO ToDTO(this Brand Brand) => Brand is null ? null : new BrandDTO
+        {
+            Id = Brand.Id,
+            Name = Brand.Name,
+        };
+
+        public static Brand FromDTO(this BrandDTO Brand) => Brand is null ? null : new Brand
+        {
+            Id = Brand.Id,
+            Name = Brand.Name,
+        };
+    }
+}
--- Services/WebStoreApp.Services/Mapping/ProductMapper.cs
+++ Services/WebStoreApp.Services/Mapping/ProductMapper.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebStoreApp.Domain.DTO;
 using WebStoreApp.Domain.Entities;
 using WebStoreApp.Domain.ViewModels;
 
@@ -9,7 +10,6 @@ namespace WebStoreApp.Services.Mapping
 {
     public static class ProductMapper
     {
-
         public static ProductViewModel ToView(this Product p) => new ProductViewModel
         {
             Id = p.Id,
@@ -22,5 +22,27 @@ namespace WebStoreApp.Services.Mapping
 
         public static IEnumerable<ProductViewModel> ToView(this IEnumerable<Product> p) => p.Select(ToView);
 
+        public static ProductDTO ToDTO(this Product p) => p is null ? null : new ProductDTO
+        {
+            Id = p.Id,
+            Name = p.Name,
+            Order = p.Order,
+            Price = p.Price,
+            ImageUrl = p.ImageUrl,
+            Brand = p.Brand.ToDTO(),
+            Section = p.Section.ToDTO(),
+        };
+
+        public static Product FromDTO(this ProductDTO p) => p is null ? null : new Product
+        {
+            Id = p.Id,
+            Name = p.Name,
+            Order = p.Order,
+            Price = p.Price,
+            ImageUrl = p.ImageUrl,
+            BrandId = p.Brand?.Id,
+            Brand = p.Brand.FromDTO(),
+            Section = p.Section.FromDTO(),
+        };
     }
 }
