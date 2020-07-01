3cff9c259e21b564e8f8ff8cdbfcce62b7a12d3b
Vacumsio <vacumsio@gmail.com>
2020-07-01 12:13:53 +0300

    константы для путей

---
+++ Common/WebStoreApp.Domain/WebApiIdentity.cs
@@ -0,0 +1,8 @@
+﻿namespace WebStoreApp.Domain
+{
+    public static class WebApiIdentity
+    {
+        public const string Roles = "api/roles";
+        public const string Users = "api/users";
+    }
+}
