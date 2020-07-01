3c47aabe4173cb75392878f8363a6fea3767c74c
Vacumsio <vacumsio@gmail.com>
2020-07-01 12:05:16 +0300

    DTO для Изера и клейма

---
+++ Common/WebStoreApp.Domain/DTO/Identity/UserDTO.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Security.Policy;
+using WebStoreApp.Domain.Entities.Identity;
+
+namespace WebStoreApp.Domain.DTO.Identity
+{
+    public abstract class UserDTO
+    {
+        public User User { get; set; }
+    }
+
+    public class AddLoginDTO: UserDTO
+    {
+        public UserLoginInfo UserLoginInfo { get; set; }
+    }
+
+    public class PasswordHashDTO : UserDTO
+    {
+        public string Hash { get; set; }
+    }
+
+    public class SetLockoutDTO: UserDTO
+    {
+        public DateTimeOffset? LockoutEnd { get; set; }
+    }
+}
