096f5c77ee2c2d8e0646139720a2db7b3ebc4d77
Vacumsio <vacumsio@gmail.com>
2020-07-09 12:54:01 +0300

    Тесты на Home / Blog

---
+++ Tests/WebStoreApp.Tests/Controllers/BlogControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebStoreApp.Controllers;
+
+using Assert = Xunit.Assert;
+
+namespace WebStoreApp.Tests.Controllers
+{
+    [TestClass]
+    public class BlogControllerTests
+    {
+        [TestMethod]
+        public void BlogList_return_View()
+        {
+            var controller = new BlogController();
+            var result = controller.BlogList();
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [TestMethod]
+        public void BlogSingle_return_View()
+        {
+            var controller = new BlogController();
+            var result = controller.BlogSingle();
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
---
+++ Tests/WebStoreApp.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebStoreApp.Controllers;
+
+using Assert = Xunit.Assert;
+
+namespace WebStoreApp.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTests
+    {
+        [TestMethod]
+        public void Index_returns_View()
+        {
+            var controller = new HomeController();
+
+            var result = controller.Index();
+
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
