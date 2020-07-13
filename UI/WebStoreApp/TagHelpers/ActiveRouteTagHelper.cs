e53d60c3a9707658258fade4c6c135bc25acc131
Vacumsio <vacumsio@gmail.com>
2020-07-13 15:08:12 +0300

    Таг хелпер для подсветки готов

--- UI/WebStoreApp/TagHelpers/ActiveRouteTagHelper.cs
+++ UI/WebStoreApp/TagHelpers/ActiveRouteTagHelper.cs
@@ -27,20 +27,45 @@ namespace WebStoreApp.TagHelpers
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             if (IsActive())
-                MakeActive();
+                MakeActive(output);
 
             output.Attributes.RemoveAll(AttributeName);
         }
 
         private bool IsActive()
         {
-            throw new NotImplementedException();
+            var route_values = ViewContext.RouteData.Values;
+            var current_controller = route_values["controller"].ToString();
+            var current_action = route_values["action"].ToString();
+
+            const StringComparison ignore_case = StringComparison.OrdinalIgnoreCase;
+
+            if (!string.IsNullOrEmpty(Controller) && string.Equals(current_controller, Controller, ignore_case))
+                return false;
+            if (!string.IsNullOrEmpty(Action) && string.Equals(current_action, Action, ignore_case))
+                return false;
+
+            foreach (var (key, value) in RouteValues)
+                if (!route_values.ContainsKey(key) || route_values[key].ToString() != value)
+                    return false;
+
+            return true;
         }
 
-        private void MakeActive()
+        private void MakeActive(TagHelperOutput output)
         {
-            throw new NotImplementedException();
-        }
+            var class_attribute = output.Attributes.FirstOrDefault(attr => attr.Name == "class");
 
+            if (class_attribute is null)
+            {
+                output.Attributes.Add("class", "active");
+            }
+            else
+            {
+                if (class_attribute.Value.ToString()?.Contains("active") ?? false)
+                    return;
+                output.Attributes.SetAttribute("class", class_attribute.Value + " active");
+            }
+        }
     }
 }
