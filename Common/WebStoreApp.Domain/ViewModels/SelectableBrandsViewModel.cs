d9d15729858f3b3f1174a09a6e714d9475bb6f1d
Vacumsio <vacumsio@gmail.com>
2020-07-13 17:48:04 +0300

    добавлена подсветка/подчеркивание к левому меню товаров бренды работают секции пока нет

---
+++ Common/WebStoreApp.Domain/ViewModels/SelectableBrandsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebStoreApp.Domain.ViewModels
+{
+    public class SelectableBrandsViewModel
+    {
+        public IEnumerable<BrandViewModel> Brands { get; set; }
+
+        public int? CurrentBrandId { get; set; }
+    }
+}
--- UI/WebStoreApp/Components/SectionsViewComponent.cs
+++ UI/WebStoreApp/Components/SectionsViewComponent.cs
@@ -10,16 +10,27 @@ namespace WebStoreApp.Components
     {
         private readonly IProductData _ProductData;
 
-        public SectionsViewComponent(IProductData ProductData)
+        public SectionsViewComponent(IProductData ProductData) => _ProductData = ProductData;
+
+        public IViewComponentResult Invoke(string SectionId)
         {
-            _ProductData = ProductData;
+            var section_id = int.TryParse(SectionId, out var id) ? id : (int?)null;
+
+            var sections = GetSections(section_id, out var parent_section_id);
+
+            return View(new SelectableSectionsViewModel
+            { 
+                Sections = sections,
+                CurrentSectionId = section_id,
+                ParentSectionId = parent_section_id
+            });
         }
 
-        public IViewComponentResult Invoke() => View(GetSections());
-
-        private IEnumerable<SectionViewModel> GetSections()
+        private IEnumerable<SectionViewModel> GetSections(int? SectionId, out int? ParentSectionId)
         {
-            var sections = _ProductData.GetSections();
+            ParentSectionId = null;
+
+            var sections = _ProductData.GetSections().ToArray();
             var parent_sections = sections.Where(s => s.ParentId is null);
 
             var parent_sections_views = parent_sections
@@ -35,6 +46,10 @@ namespace WebStoreApp.Components
             {
                 var childs = sections.Where(s => s.ParentId == parent_section.Id);
                 foreach (var child_sections in childs)
+                {
+                    if (child_sections.Id == SectionId)
+                        ParentSectionId = parent_section.Id;
+                    
                     parent_section.ChildSections.Add(new SectionViewModel
                     {
                         Id = child_sections.Id,
@@ -42,6 +57,8 @@ namespace WebStoreApp.Components
                         Order = child_sections.Order,
                         ParentSection = parent_section
                     });
+                }
+                    
                 parent_section.ChildSections.Sort((a, b) => Comparer<double>.Default.Compare(a.Order, b.Order));
             }
 
