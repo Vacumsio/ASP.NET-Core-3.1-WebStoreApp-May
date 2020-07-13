ad8beece6eae5e430fd778cb80ec6d37ae8881cf
Vacumsio <vacumsio@gmail.com>
2020-07-13 16:42:26 +0300

    все работает но наоборот =)

--- UI/WebStoreApp/TagHelpers/ActiveRouteTagHelper.cs
+++ UI/WebStoreApp/TagHelpers/ActiveRouteTagHelper.cs
@@ -11,7 +11,10 @@ namespace WebStoreApp.TagHelpers
     [HtmlTargetElement(Attributes = AttributeName)]
     public class ActiveRouteTagHelper : TagHelper
     {
+        #region Поля класса
+
         public const string AttributeName = "is-active-route";
+        public const string IgnoreActionName = "ignore-action";
 
         [HtmlAttributeName("asp-action")]
         public string Action { get; set; }
@@ -19,20 +22,27 @@ namespace WebStoreApp.TagHelpers
         [HtmlAttributeName("asp-controller")]
         public string Controller { get; set; }
 
-        [HtmlAttributeName("asp-all-route-data",DictionaryAttributePrefix ="asp-route-")]
+        [HtmlAttributeName("asp-all-route-data", DictionaryAttributePrefix = "asp-route-")]
         public IDictionary<string, string> RouteValues { get; set; } = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
 
         [ViewContext, HtmlAttributeNotBound]
-        public  ViewContext ViewContext { get; set; }
+        public ViewContext ViewContext { get; set; }
+
+        #endregion
+
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
-            if (IsActive())
+            var ignore_name = output.Attributes.ContainsName(IgnoreActionName);
+
+            if (IsActive(ignore_name))
                 MakeActive(output);
 
+            output.Attributes.RemoveAll(IgnoreActionName);
             output.Attributes.RemoveAll(AttributeName);
         }
 
-        private bool IsActive()
+        #region Методы отображение таг хэлпера
+        private bool IsActive(bool IgnoreAction)
         {
             var route_values = ViewContext.RouteData.Values;
             var current_controller = route_values["controller"].ToString();
@@ -42,7 +52,7 @@ namespace WebStoreApp.TagHelpers
 
             if (!string.IsNullOrEmpty(Controller) && string.Equals(current_controller, Controller, ignore_case))
                 return false;
-            if (!string.IsNullOrEmpty(Action) && string.Equals(current_action, Action, ignore_case))
+            if (!IgnoreAction && !string.IsNullOrEmpty(Action) && string.Equals(current_action, Action, ignore_case))
                 return false;
 
             foreach (var (key, value) in RouteValues)
@@ -52,7 +62,7 @@ namespace WebStoreApp.TagHelpers
             return true;
         }
 
-        private void MakeActive(TagHelperOutput output)
+        private static void MakeActive(TagHelperOutput output)
         {
             var class_attribute = output.Attributes.FirstOrDefault(attr => attr.Name == "class");
 
@@ -67,5 +77,21 @@ namespace WebStoreApp.TagHelpers
                 output.Attributes.SetAttribute("class", class_attribute.Value + " active");
             }
         }
+
+        //private static void MakeActive(TagHelperOutput output)
+        //{
+        //    var class_attribute = output.Attributes.FirstOrDefault(attr => attr.Name == "class");
+
+        //    if (class_attribute is null)
+        //        output.Attributes.Add("class", "active");
+        //    else
+        //    {
+        //        if (class_attribute.Value.ToString()?.Contains("active") ?? false) 
+        //            return;
+        //        output.Attributes.SetAttribute("class", class_attribute.Value + " active");
+        //    }
+        //}
+        #endregion
+
     }
 }
