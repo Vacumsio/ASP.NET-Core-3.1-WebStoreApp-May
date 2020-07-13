a351514310ceec37b96c242f8f8ce4e8c670b359
Vacumsio <vacumsio@gmail.com>
2020-07-13 13:48:03 +0300

    Заготовка тагхелпера

---
+++ UI/WebStoreApp/TagHelpers/ActiveRouteTagHelper.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStoreApp.TagHelpers
+{
+    [HtmlTargetElement(Attributes = AttributeName)]
+    public class ActiveRouteTagHelper : TagHelper
+    {
+        public const string AttributeName = "is-active-route";
+
+        [HtmlAttributeName("asp-action")]
+        public string Action { get; set; }
+
+        [HtmlAttributeName("asp-controller")]
+        public string Controller { get; set; }
+
+        [HtmlAttributeName("asp-all-route-data",DictionaryAttributePrefix ="asp-route-")]
+        public IDictionary<string, string> RouteValues { get; set; } = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+
+        [ViewContext, HtmlAttributeNotBound]
+        public  ViewContext ViewContext { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (IsActive())
+                MakeActive();
+
+            output.Attributes.RemoveAll(AttributeName);
+        }
+
+        private bool IsActive()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MakeActive()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
