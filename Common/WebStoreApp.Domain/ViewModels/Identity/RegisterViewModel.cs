9b72052dc425c35a360f5701158d9f421220920e
Vacumsio <vacumsio@gmail.com>
2020-07-20 19:22:03 +0300

    Настроена асинхронная валидация идентити, либман++

--- Common/WebStoreApp.Domain/ViewModels/Identity/RegisterViewModel.cs
+++ Common/WebStoreApp.Domain/ViewModels/Identity/RegisterViewModel.cs
@@ -1,12 +1,15 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WebStoreApp.Domain.ViewModels.Identity
 {
     public class RegisterViewModel
     {
         [Required]
+        [MinLength(3, ErrorMessage = ("Минимальная длина 3 символа"))]
         [MaxLength(256)]
         [Display(Name = "Введите Имя")]
+        [Remote("IsNameFree","Account")]
         public string UserName { get; set; }
         [Required]
         [DataType(DataType.Password)]
